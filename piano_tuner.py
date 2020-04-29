58298d9ec27cb367d1834b92b3c7584fb1ed8d6b
Guillaume Bertello <guillaume.bertello@gmail.com>
2020-04-29 12:44:00 +0200

    New version with zoom on multiple frequencies

--- piano_tuner.py
+++ piano_tuner.py
@@ -9,7 +9,7 @@ duration = 5.5
 start = 1.5
 rate = 44100
 
-last_action = ""
+last_action = "49"
 
 def record():
   y = sd.rec(int(duration * rate), samplerate=rate, channels=1)
@@ -21,22 +21,36 @@ def record():
   yf = np.absolute(fft(y[int(rate*start):int(rate*duration)]))
   yf = yf/np.amax(yf)
   xf = np.arange(yf.size)*1.0/yf.size*rate
-
+  
   yf = yf[:int(yf.size/2)]
   xf = xf[:int(xf.size/2)]
 
   return x, y, xf, yf
 
 while True:
-  print("Enter action: (p)itch / (f)ourier / (s)how / (z)oom / (d)iapason / (q)uit")
+  print("Enter action: note number / (f)ourier / (q)uit")
   key = input()
   if key == "":
     key = last_action
- 
-  if key.lower() in ["p"]:
+  if key[0] in [str(i) for i in range(10)]:
     x, y, xf, yf = record()
-    pitch = xf[np.where(yf == np.amax(yf))[0]]
-    print("Pitch: %s" % pitch)
+    t = int(key)
+    n_harmonics = 8
+    harmonics = 440*np.power(2, (t - 49)/12)*np.arange(1, n_harmonics + 1)
+    subplots(4, 2, figsize=((14,7)))
+    for i in range(n_harmonics):
+      if harmonics[i] < rate/2:
+        subplot(4, 2, i + 1)
+        center = int(xf.size/(rate/2)*harmonics[i])
+        delta = int(xf.size/(rate/2)*0.1*harmonics[0])
+        xf_zoom = xf[(center - delta):(center + delta)]
+        yf_zoom = yf[(center - delta):(center + delta)]
+        vlines(xf_zoom,[0],yf_zoom,'r')
+        xlabel('f (Hz)')
+        ylabel('A')
+        grid()
+        axis([xf_zoom[0],xf_zoom[-1],0,np.amax(yf_zoom)])
+    show()
   elif key.lower() in ["f"]:  
     x, y, xf, yf = record()
     figure(figsize=(12,4))
@@ -46,33 +60,6 @@ while True:
     grid()
     axis([0,rate/2,0,1])
     show()
-  elif key.lower().startswith("z"):
-    x, y, xf, yf = record()
-    f = int(key[1:])
-    center = int(xf.size/(rate/2)*f)
-    delta = int(xf.size/(rate/2)*30)
-    xf_zoom = xf[(center - delta):(center + delta)]
-    yf_zoom = yf[(center - delta):(center + delta)]
-    figure(figsize=(12,4))
-    vlines(xf_zoom,[0],yf_zoom,'r')
-    xlabel('f (Hz)')
-    ylabel('A')
-    grid()
-    axis([xf_zoom[0],xf_zoom[-1],0,np.amax(yf_zoom)*1.2])
-    show()
-  elif key.lower() == "s":
-    x, y, xf, yf = record()
-    max = np.amax(np.absolute(y))
-    figure(figsize=(12,4))
-    plot(x,y,'r')
-    xlabel('t (s)')
-    ylabel('A')
-    grid()
-    axis([0, duration, max*-1.2, max*1.2])
-    show()
-  elif key.lower() == "d":
-    t = np.linspace(0, duration, int(rate * duration))
-    sd.play(np.sin(440 * 2 * np.pi * t), rate)
   elif key.lower() == "q":
     sys.exit(0)  
 
