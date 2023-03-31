9df46753147bbc0de7e67e9a88ea7a7cffd77ddd
Guillaume Bertello <guillaume.bertello@gmail.com>
2023-03-31 16:44:12 +0200

    update print_cache

--- print_cache.py
+++ print_cache.py
@@ -12,9 +12,5 @@ harmonics = cache.get("harmonics")
 for p in sorted([int(x) for x in harmonics.keys()]):
   pitch = str(pitches.get_pitch_value(str(p)))
   values = [str(x) for x in harmonics[str(p)]]
-  values2 = values.copy()
-
-  for i in range(0, len(values)):
-    values2[i] = str(1200 * math.log2(float(values[i]) / ((i + 1) * float(values[0])))).ljust(20)
-  print("\t".join([pitch] + values2))
+  print("\t".join([pitch] + values))
 
