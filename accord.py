23b16e5aa2963fd3888833b267a9b9316c9a6eec
Guillaume BERTELLO <guillaume.bertello@gmail.com>
2023-07-22 18:00:01 +0200

    Decouple Stream object from App

--- accord.py
+++ accord.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+try:
+  import tkinter
+except:
+  print("Tkinter needs to be installed on the system")
+  print("brew install python-tk")
+  sys.exit(0)
+
 from animation import *
 from cache import *
 from figure import *
@@ -7,11 +15,12 @@ from pitches import *
 from stream import *
 from window import *
 
+
 class App(Tk):
   def __init__(self):
     self.pitches = Pitches()
     self.cache = Cache()
-    self.stream = Stream(self.cache)
+    self.stream = Stream(self.cache.get("duration"), self.cache.get("samplerate"), self.cache.get("device"), self.cache.get("channels"))
     self.figure = Figure(self.pitches, self.cache, self.stream)
     self.window = Window(self.pitches, self.cache, self.stream, self.figure)
     self.ani = Animation(self.cache, self.figure) # Need the figure to be declared in window canvas before starting animation
--- stream.py
+++ stream.py
@@ -2,15 +2,18 @@ import sounddevice as sd
 import numpy as np
 
 class Stream(sd.InputStream):
-  def __init__(self, cache):
-    self.cache = cache
+  def __init__(self, duration, init_samplerate, init_device, init_channels):
+    self.duration = duration
+    self.init_samplerate = init_samplerate
+    self.init_device = init_device
+    self.init_channels = init_channels
     self.reset()
 
   def reset(self):
-    self.y = np.zeros(int(self.cache.get("duration") * self.cache.get("samplerate")))
+    self.y = np.zeros(int(self.duration * self.init_samplerate))
     if self.active:
       self.close()
-    super().__init__(device=self.cache.get("device"), channels=self.cache.get("channels"), samplerate=self.cache.get("samplerate"), callback=lambda indata, *args: self.audio_callback(indata))
+    super().__init__(device=self.init_device, channels=self.init_channels, samplerate=self.init_samplerate, callback=lambda indata, *args: self.audio_callback(indata))
 
   def audio_callback(self, indata):
     data = indata[:]
